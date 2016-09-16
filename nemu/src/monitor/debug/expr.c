a643d144d01d466deebfce6c9bbb48623393274b
tracer-ics2016 <tracer@njuics.org>
2016-09-16 13:03:08 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  13:03:08 up  3:32,  1 user,  load average: 0.01, 0.01, 0.00 767130dc3727f5a8a8ee1462e46d7a99ac758421

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -76,7 +76,7 @@ static bool make_token(char *e) {
 
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
-	 	for(i = 0; i < NR_REGEX;  i ++) {
+ 	 	for(i = 0; i < NR_REGEX;  i ++) {
  	 		if(regexec(&re[i], e  + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
@@ -88,9 +88,9 @@ static bool make_token(char *e) {
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array `tokens'. For certain types
 				 * of tokens, some extra actions should be performed.
-		 		 */
+ 		 		 */
 
-				switch(rules[i].token_type) {
+ 				switch(rules[i].token_type) {
 					case EQ:
 						tokens[nr_token].type = EQ;
 						break;
@@ -140,12 +140,12 @@ static bool make_token(char *e) {
 						tokens[nr_token].type = REG;
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
 						tokens[nr_token].str[substr_len] = '\0';
-						
+						break;
 					default: panic("please implement me");
   	 	 		}
 				++nr_token;
 				break;
- 			}
+  			}
   		}
 
  		if(i == NR_REGEX) {
