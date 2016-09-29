a38e0ef3d64cd311cf27b0fb4e196d4e05a48dc0
tracer-ics2016 <tracer@njuics.org>
2016-09-29 10:13:57 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:13:57 up  1:40,  1 user,  load average: 0.08, 0.03, 0.01 831ab0c5d6c1f7df31750844b4899fa77a64905

---
+++ nemu/src/cpu/exec/call/call-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+//#if DATA_BYTE == 4
+static void do_execute()
+{
+	REG(R_ESP) += DATA_BYTE;
+	MEM_W(REG(R_ESP), cpu.eip);
+	cpu.eip = op_src->imm + cpu.eip;	
+}
+
+make_instr_helper(i)
+//#endif
+
+
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/call/call.h
@@ -0,0 +1,8 @@
+#ifndef _CALL_H_
+#define _CALL_H_
+
+make_helper(call_i_l);
+make_helper(call_i_w);
+
+
+#endif
