957b6de18fda0977dbbfe9c633329b0ff09ef86f
tracer-ics2016 <tracer@njuics.org>
2016-10-01 01:39:47 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:39:47 up 14 min,  2 users,  load average: 0.08, 0.04, 0.07 7280e9e96ede303418c6c760851e11181720d8a1

---
+++ nemu/src/cpu/exec/call/test-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+
+#define instr test
+
+static void do_execute()
+{
+	op_src->val && op->src2->val;
+	//cpu.	
+}
+
+make_instr_helper()
+
+#include "cpu/exec/template.end.h"
