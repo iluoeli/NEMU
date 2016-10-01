7f67f9fce477311575ff1fc1a2ea799860e4e875
tracer-ics2016 <tracer@njuics.org>
2016-10-01 05:31:44 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  05:31:44 up  4:05,  2 users,  load average: 0.00, 0.00, 0.00 a85472c84b5f0fa781451cf2d37c507f7b6d683

---
+++ nemu/src/cpu/exec/transfer/jz-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr je
+
+static void do_execute()
+{
+	if(cpu.EFLAGES.ZF == 1)
+		cpu.eip += op_src->val & 0xff;	
+		print_asm_template2();
+}
+
+make_instr_helper(i)
+
+#include "cpu/exec/template-end.h"
