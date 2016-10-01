405493d123628d4de282f28f5f11d98312c2cfcd
tracer-ics2016 <tracer@njuics.org>
2016-10-01 01:46:20 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:46:20 up 20 min,  2 users,  load average: 0.00, 0.00, 0.03 937016c9b85e6645f1aed6f6e9b425a9d52df72b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,23 +14,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef	struct {
-	//General Registers define here
-	union {
- 		union {
-			uint32_t _32;
-			uint16_t _16;
-			uint8_t _8[2];
-  			} gpr[8];
-
-	/* Do NOT change the order of the GPRs' definitions. */
-   		struct {
-			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		};
-	};
-	//EIP
-	swaddr_t eip;
-	struct {
+typedef struct {
 		uint32_t CF:1;
 		uint32_t :1;
 		uint32_t PF:1;
@@ -48,7 +32,25 @@ typedef	struct {
 		uint32_t :1;
 		uint32_t RF:1;
 		uint32_t VM:1;		
+}Eflages;
+typedef	struct {
+	//General Registers define here
+	union {
+ 	 	union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+  			} gpr[8];
+
+	/* Do NOT change the order of the GPRs' definitions. */
+   		 struct {
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
 	};
+	
+	Eflages EFLAGES;	
+	//EIP
+	swaddr_t eip;
 } CPU_state;
 
 extern CPU_state cpu;
