d689710926420da42b7fce911a75fa8661258a66
tracer-ics2016 <tracer@njuics.org>
2016-10-02 11:22:26 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:22:26 up 9 min,  2 users,  load average: 0.08, 0.09, 0.09 19b6a16c3be71b144c32aff068ee50d6bcf594

--- nemu/src/cpu/exec/transfer/call-template.h
+++ nemu/src/cpu/exec/transfer/call-template.h
@@ -6,8 +6,8 @@
 static void do_execute()
 {
 	REG(R_ESP) -= DATA_BYTE;
-	MEM_W(REG(R_ESP), cpu.eip+1+DATA_BYTE);
-	cpu.eip += op_src->imm;	
+	MEM_W(REG(R_ESP), (cpu.eip+1+DATA_BYTE) & (0xff<<DATA_BYTE));
+	cpu.eip = (op_src->val + cpu.eip) & (0xff<<DATA_BYTE);	
 	print_asm_template1();
 }
 
