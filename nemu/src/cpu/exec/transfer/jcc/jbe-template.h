0ce876da133d30f9c4df0a879ad223e98a501781
tracer-ics2016 <tracer@njuics.org>
2016-10-03 15:47:55 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  15:47:55 up  7:32,  2 users,  load average: 0.01, 0.01, 0.00 ca150c08c78dbd93d61795847cc28b83b204c583

--- nemu/src/cpu/exec/transfer/jcc/jbe-template.h
+++ nemu/src/cpu/exec/transfer/jcc/jbe-template.h
@@ -1,14 +1,24 @@
 #include "cpu/exec/template-start.h"
 
+#if DATA_BYTE == 1
+#define RET_DATA_TYPE int8_t
+#elif DATA_BYTE == 2
+#define RET_DATA_TYPE int16_t
+#elif DATA_BYTE == 4
+#define RET_DATA_TYPE int32_t
+#endif
+
 #define instr jbe
 
 static void do_execute()
 {
 	if(cpu.EFLAGES.ZF || cpu.EFLAGES.CF)
-		cpu.eip += (int8_t)op_src->imm;	
+		cpu.eip += (RET_DATA_TYPE)op_src->imm;	
 	print_asm_template1();
 }
 
 make_instr_helper(i)
+
+#undef RET_DATA_TYPE
 
 #include "cpu/exec/template-end.h"
