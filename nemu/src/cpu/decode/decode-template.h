ba294cf5e0a4ba61696c77e82e4436acb00ce4d1
tracer-ics2016 <tracer@njuics.org>
2016-10-04 12:49:00 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:49:00 up  1:48,  2 users,  load average: 0.15, 0.04, 0.01 aab617af709577f4a26848a629f32d335d817ddc

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -33,9 +33,9 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	op_src->simm = ???
 	 */
 	 op_src->simm = instr_fetch(eip, DATA_BYTE);
-	panic("please implement me");
+//	panic("please implement me");
 
-	op_src->val = (int32_t)op_src->simm;
+	op_src->val = (DATA_TYPE_S)op_src->simm;
 
 #ifdef DEBUG
 	snprintf(op_src->str, OP_STR_SIZE, "$0x%x", op_src->val);
