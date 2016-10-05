e42613af47eb4c68b7b502f3e04d67b216e81313
tracer-ics2016 <tracer@njuics.org>
2016-10-05 16:14:39 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  16:14:39 up 13:41,  2 users,  load average: 0.06, 0.02, 0.00 5f1cc5f195cf6772b509b48cd6472169dfd41cfc

--- testcase/src/bit.c
+++ testcase/src/bit.c
@@ -31,17 +31,26 @@ int main() {
 	nemu_assert(getbit(buf, 4) == 0);
 	set_bp();
 	nemu_assert(getbit(buf, 5) == 1);
+	set_bp();
 	nemu_assert(getbit(buf, 6) == 0);
+	set_bp();
 	nemu_assert(getbit(buf, 7) == 1);
 
 //	set_bp();
 	setbit(buf, 8, 1);
+	set_bp();
 	setbit(buf, 9, 0);
+	set_bp();
 	setbit(buf, 10, 1);
+	set_bp();
 	setbit(buf, 11, 0);
+	set_bp();
 	setbit(buf, 12, 1);
+	set_bp();
 	setbit(buf, 13, 0);
+	set_bp();
 	setbit(buf, 14, 1);
+	set_bp();
 	setbit(buf, 15, 0);
 	nemu_assert(buf[1] == 0x55);
 
