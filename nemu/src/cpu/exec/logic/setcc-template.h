d7a71a94ebd876540294e1e382e6250d0331b638
tracer-ics2016 <tracer@njuics.org>
2016-10-05 09:03:12 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:03:12 up  6:30,  2 users,  load average: 0.05, 0.02, 0.00 afe78992736e2527ab7fd9bd857e823f0016640

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -1,9 +1,9 @@
 #include "cpu/exec/template-start.h"
 
 #define instr setne
-make_helper(setne_i_b)
+make_helper(setne_rm_b)
 {
-	int len = decode_i_b(eip);
+	int len = decode_rm_b(eip);
 	DATA_TYPE result = (cpu.EFLAGES.ZF == 0);
 	OPERAND_W(op_src, result);
 	return len+1;	
--- nemu/src/cpu/exec/logic/setcc.h
+++ nemu/src/cpu/exec/logic/setcc.h
@@ -1,7 +1,7 @@
 #ifndef _SETCC_H_
 #define _SETCC_H_
 
-make_helper(setne_i_b);
+make_helper(setne_rm_b);
 
 
 #endif
