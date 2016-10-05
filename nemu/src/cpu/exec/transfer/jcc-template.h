1272f3e2d2573af91c6ba681921c53c1fd3261d4
tracer-ics2016 <tracer@njuics.org>
2016-10-05 14:48:38 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  14:48:38 up 12:15,  2 users,  load average: 0.00, 0.00, 0.00 befb5f5dde5275535ae032fecb32ba22bc271cce

--- nemu/src/cpu/exec/transfer/jcc-template.h
+++ nemu/src/cpu/exec/transfer/jcc-template.h
@@ -3,7 +3,7 @@
 #define instr jne
 make_helper(concat(jne_i_, SUFFIX))
 {
-	uint32_t len = concat(decode_i_, SUFFIX)(eip);
+	uint32_t len = concat(decode_i_, SUFFIX)(eip+1);
 	if(cpu.EFLAGES.ZF == 0)
 		cpu.eip = cpu.eip +  op_src->val;	
 	print_asm_template1();
