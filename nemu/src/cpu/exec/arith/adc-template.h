fcb33072ab689cd34268b995cca7219b8fad4d56
tracer-ics2016 <tracer@njuics.org>
2016-10-06 05:15:02 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  05:15:02 up  3:29,  2 users,  load average: 0.02, 0.01, 0.00 5a82c5ac8e17664363e4c082b2cb0f4d1a4464fc

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,31 @@
+#include "cpu/exec/template-start.h"
+
+#if DATA_BYTE == 1
+#define RET_DATA_TYPE int8_t
+#elif DATA_BYTE == 2
+#define RET_DATA_TYPE int16_t
+#elif DATA_BYTE == 4
+#define RET_DATA_TYPE int32_t
+#endif
+
+#define instr adc
+
+static void do_execute()
+{
+	DATA_TYPE result = op_dest->val + op_src->val + cpu.EFLAGES.CF;
+	OPERAND_W(op_dest, result);
+	print_asm_template2();
+}
+
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+#undef RET_DATA_TYPE
+
+#include "cpu/exec/template-end.h"
