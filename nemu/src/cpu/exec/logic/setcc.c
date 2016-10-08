f530fcaecd6076d85fd8a596dab4925a3b1d601e
tracer-ics2016 <tracer@njuics.org>
2016-10-08 13:35:44 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  13:35:44 up  1:59,  2 users,  load average: 0.09, 0.04, 0.01 fe1b2725cf0f805c88cd5b65ffed5f204081f122

--- nemu/src/cpu/exec/logic/setcc.c
+++ nemu/src/cpu/exec/logic/setcc.c
@@ -3,7 +3,7 @@
 #define DATA_BYTE 1
 
 #define instr setne
-#define instr_condition (cpu.EFLAGES.CF == 0)
+#define instr_condition (cpu.EFLAGES.ZF == 0)
 #include "setcc-template.h"
 #undef instr
 #undef instr_condition
