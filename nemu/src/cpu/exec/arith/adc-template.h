13fbaca63a0ce21102b3e4f124b62047358a6b78
tracer-ics2016 <tracer@njuics.org>
2016-10-09 12:31:30 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:31:30 up  4:11,  2 users,  load average: 0.00, 0.00, 0.00 b9808bf4742471d592a9b1b7df3a1cd3a6487633

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -12,8 +12,19 @@
 
 static void do_execute()
 {
-	DATA_TYPE result = op_dest->val + op_src->val + cpu.EFLAGES.CF;
+	uint64_t result = op_dest->val + op_src->val + cpu.EFLAGES.CF;
 	OPERAND_W(op_dest, result);
+	cpu.EFLAGES.OF =  ( MSB(op_src->val) == MSB(op_dest->val+1) && (MSB(op_dest->val) != MSB(result) ));
+	cpu.EFLAGES.SF = MSB(result);
+	cpu.EFLAGES.ZF = ((DATA_TYPE)result == 0);
+	uint8_t ret = (result>>4) ^ result;
+	ret = (ret>>2) ^ ret;
+	ret = (ret>>1) ^ ret;
+	cpu.EFLAGES.PF = ret & 1;
+	cpu.EFLAGES.CF = (result >> (DATA_BYTE*8 - 1)) & 1;
+			 
+
+
 	print_asm_template2();
 }
 
