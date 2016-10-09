c17b6ed7fe045e30ed34b7bf5b901efd97d72b7f
tracer-ics2016 <tracer@njuics.org>
2016-10-09 10:07:54 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:07:54 up  1:47,  2 users,  load average: 0.00, 0.00, 0.00 222d700b730fa00310557d56867addd1c371bbc7

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -3,11 +3,19 @@
 #define instr inc
 
 static void do_execute () {
-	DATA_TYPE result = op_src->val + 1;
+	uint64_t result = op_src->val + 1;
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
 	panic("please implement me");
+    cpu.EFLAGES.OF =  ( MSB(op_src->val) == 0 && (MSB(op_src->val) != MSB(result)));
+	cpu.EFLAGES.SF = MSB(result) & 1;
+    cpu.EFLAGES.ZF = ((DATA_TYPE)result == 0);
+    uint8_t ret = (result>>4) ^ result;
+	ret = (ret>>2) ^ ret;
+	ret = (ret>>1) ^ ret;
+	cpu.EFLAGES.PF = ret & 1;
+	cpu.EFLAGES.CF = (result >> (DATA_BYTE*8 - 1)) & 1;
 
 	print_asm_template1();
 }
