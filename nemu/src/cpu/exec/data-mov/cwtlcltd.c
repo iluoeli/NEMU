284cd49fd0290640e8c79827b62dd48da3054711
tracer-ics2016 <tracer@njuics.org>
2016-10-10 09:47:35 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:47:35 up  7:32,  2 users,  load average: 1.00, 1.00, 1.00 e84413da931202f09cac4d323711cb25a050d152

---
+++ nemu/src/cpu/exec/data-mov/cwtlcltd.c
@@ -0,0 +1,18 @@
+#include "cpu/exec/helper.h"
+
+make_helper(cwtl)
+{
+	int32_t tmp = reg_w(R_EAX);
+	reg_l(R_EAX) = tmp;
+	print_asm("cwtl");	
+	return 1;
+}
+
+
+make_helper(cltd)
+{
+	int64_t tmp = reg_l(R_EAX);
+	reg_l(R_EDX) = (tmp >> 32) & 0xffffffff;
+	print_asm("cltd");	
+	return 1;
+}
---
+++ nemu/src/cpu/exec/data-mov/cwtlcltd.h
@@ -0,0 +1,9 @@
+#ifndef _CWTL/CLTD_H_
+#define _CWTL/CLTD_H_
+
+make_helper(cwtl);
+make_helper(cltd);
+		  
+		
+#endif
+			    
