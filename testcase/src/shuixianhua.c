cbc8225aec94fcdf35297b520407af0a64bfed30
tracer-ics2016 <tracer@njuics.org>
2016-10-10 08:19:22 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:19:22 up  6:04,  2 users,  load average: 0.00, 0.00, 0.00 a527f88c7ac2e77433de4c832801af79635b7de

--- testcase/src/shuixianhua.c
+++ testcase/src/shuixianhua.c
@@ -10,9 +10,9 @@ int main() {
 	int n, n2, n1, n0;
 	int k = 0;
 	for(n = 100; n < 1000; n ++) {
-		set_bp();/*
+//		set_bp();
 		if(n == 153)
-			set_bp();*/
+			set_bp();
 		n2 = n / 100;
 		n1 = (n / 10) % 10;
 		n0 = n % 10;
