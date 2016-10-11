3b7e35fa96df00c88e272200ca1b80c85972899d
tracer-ics2016 <tracer@njuics.org>
2016-10-11 10:45:42 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:45:42 up  8:36,  2 users,  load average: 1.08, 1.03, 1.01 ccda8a041429076deed36f458a9e2c91998ce3ac

--- nemu/src/cpu/exec/transfer/jmp-template.h
+++ nemu/src/cpu/exec/transfer/jmp-template.h
@@ -2,12 +2,31 @@
 
 #define instr jmp
 
+
+//jmp direct 
 static void do_execute()
 {
 	cpu.eip += op_src->val;
+	if(ops_decoded.is_operand_size_16 == true)
+		cpu.eip = cpu.eip & 0x0000ffff;
 	print_asm_template1();
 }
 
 make_instr_helper(i)
+
+//jmp indirect
+make_helper(concat(jmp_rm_, SUFFIX))
+{
+	int len = concat(decode_rm_, SUFFIX)(eip);
+	if(ops_decoded.is_operand_size_16 == true){
+		cpu.eip = reg_l(op_src->reg) & 0x0000ffff;
+	}
+	else
+		cpu.eip = reg_l(op_src->reg);
+	print_asm_template1();
+	return 1+len;
+}
+
+
 
 #include "cpu/exec/template-end.h"
