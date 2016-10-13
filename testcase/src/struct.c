699bb187b52fd5ad0af392d483097ae513ec461d
tracer-ics2016 <tracer@njuics.org>
2016-10-13 01:36:40 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:36:40 up 13:04,  2 users,  load average: 1.00, 1.00, 1.00 eed03456b2cf15b7edfc5c7540ca6119904d28da

--- testcase/src/struct.c
+++ testcase/src/struct.c
@@ -17,10 +17,10 @@ int main() {
 		d.pad1[i] = i + 128;
 		d.pad2[i] = i;
 	}
-
+	set_bp();
 	struct dummy t = fun(d);
 
-	for(i = 0; i < N; i ++) {
+ 	for(i = 0; i < N; i ++) {
 		nemu_assert(t.pad1[i] == i + 128);
 		nemu_assert(t.pad2[i] == i);
 	}
