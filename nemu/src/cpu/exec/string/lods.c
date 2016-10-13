e63613207b7cba7d530b850b34648dde9c0ad678
tracer-ics2016 <tracer@njuics.org>
2016-10-13 12:57:22 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:57:22 up 14:36,  2 users,  load average: 1.00, 1.00, 1.00 4deb56d62e2ff351be565b45dfd4c97ebfce467d

--- nemu/src/cpu/exec/string/lods.c
+++ nemu/src/cpu/exec/string/lods.c
@@ -1,19 +1,50 @@
 #include "cpu/exec/helper.h"
 #include "cpu/decode/modrm.h"
 
-#define instr lods
 
-make_helper(lods)
+make_helper(lods_b)
 {
- 	if(ops_decoded.is_operand_size_16 == true) {
+  	if(ops_decoded.is_operand_size_16 == true) {
 		reg_b(R_AL) = instr_fetch(reg_w(R_SI), 1);
 		reg_w(R_SI) += cpu.EFLAGES.DF ? -1:1;
 	}
-	else{
+ 	else{
 		reg_b(R_AL) = instr_fetch(reg_l(R_ESI), 1);
 		reg_l(R_SI) += cpu.EFLAGES.DF ? -1:1;
 	}
-	print_asm("lods");
+	print_asm("lodsb");
 	return 1;
 }
 
+make_helper(lods_w)
+{
+  	if(ops_decoded.is_operand_size_16 == true) {
+		reg_w(R_AX) = instr_fetch(reg_w(R_SI), 2);
+		reg_w(R_SI) += cpu.EFLAGES.DF ? -2:2;
+	}
+ 	else{
+		reg_w(R_AX) = instr_fetch(reg_l(R_ESI), 2);
+		reg_l(R_SI) += cpu.EFLAGES.DF ? -2:2;
+	}
+	print_asm("lodsw");
+	return 1;
+}
+
+make_helper(lods_l)
+{
+  	if(ops_decoded.is_operand_size_16 == true) {
+		reg_l(R_EAX) = instr_fetch(reg_w(R_SI), 4);
+		reg_w(R_SI) += cpu.EFLAGES.DF ? -4:4;
+	}
+ 	else{
+		reg_l(R_EAX) = instr_fetch(reg_l(R_ESI), 4);
+		reg_l(R_SI) += cpu.EFLAGES.DF ? -4:4;
+	}
+	print_asm("lodsl");
+	return 1;
+}
+
+make_helper(lods_v)
+{
+	return (ops_decoded.is_operand_size_16?lods_w:lods_l)(eip);	
+}
