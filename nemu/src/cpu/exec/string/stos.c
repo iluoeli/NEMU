14bb9cecdcfa7a26bf46c442a6617ddb21fc7202
tracer-ics2016 <tracer@njuics.org>
2016-10-13 02:07:45 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  02:07:45 up 13:36,  2 users,  load average: 1.35, 1.09, 1.03 96a41bce652a1b3669a8205b7260051b862cc09

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -2,14 +2,14 @@
 
 make_helper(stos)
 {
-	if(ops_decoded.is_operand_size_16 == true)
-		reg_w(R_DI) = reg_w(R_AL);
-	else
-		reg_l(R_EDI) = reg_b(R_AL);
-	if(cpu.EFLAGES.DF == 0)
-		reg_l(R_EDI) ++;
-	else
-		reg_l(R_EDI) --;
+	if(ops_decoded.is_operand_size_16 == true) {
+		swaddr_write(reg_w(R_DI), reg_b(R_AL), 1);
+		reg_w(R_DI) += cpu.EFLAGES.DF?-1:1;
+	}
+	else{
+		swaddr_write(reg_l(R_EDI), reg_b(R_AL), 1);
+		reg_l(R_EDI) += cpu.EFLAGES.DF?-1:1;
+	}
 	
 	print_asm("stos");
 	return 1;
