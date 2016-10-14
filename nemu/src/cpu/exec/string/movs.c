7059da5238e838982798b76e64fbb2fe5eb66002
tracer-ics2016 <tracer@njuics.org>
2016-10-14 06:33:26 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  06:33:26 up 17:30,  2 users,  load average: 2.08, 1.98, 1.65 e6ef1f0d346ed4fe03dc6527da38ae21721034f

--- nemu/src/cpu/exec/string/movs.c
+++ nemu/src/cpu/exec/string/movs.c
@@ -32,7 +32,7 @@ make_helper(movs_w)
 
 make_helper(movs_l)
 {
-	swaddr_write(reg_l(R_EDI), 4, instr_fetch(reg_l(R_ESI), 4));
+	swaddr_write(reg_l(R_EDI), 4, swaddr_read(reg_l(R_ESI), 4));
  	if(cpu.EFLAGES.DF == 0){
 		reg_l(R_EDI) += 4;
 		reg_l(R_ESI) += 4;
