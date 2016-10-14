bdab5d25efd29f8cf6c31385762a99b03cc83e9f
tracer-ics2016 <tracer@njuics.org>
2016-10-14 06:13:57 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  06:13:57 up 17:10,  2 users,  load average: 1.02, 1.02, 1.00 d4df4aaa2fa5c3b7351b1dbaff4453c4e4d51eaf

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -2,14 +2,8 @@
 
 make_helper(stos_b)
 {
-	if(ops_decoded.is_operand_size_16 == true) {
-		swaddr_write(reg_w(R_DI), reg_b(R_AL), 1);
-		reg_w(R_DI) += cpu.EFLAGES.DF?-1:1;
-	}
-	else{
-		swaddr_write(reg_l(R_EDI), reg_b(R_AL), 1);
-		reg_l(R_EDI) += cpu.EFLAGES.DF?-1:1;
-	}
+	swaddr_write(reg_l(R_EDI), reg_b(R_AL), 1);
+	reg_l(R_EDI) += cpu.EFLAGES.DF?-1:1;
 	
 	print_asm("stosb");
 	return 1;
@@ -17,14 +11,8 @@ make_helper(stos_b)
 
 make_helper(stos_w)
 {
-	if(ops_decoded.is_operand_size_16 == true) {
-		swaddr_write(reg_w(R_DI), reg_w(R_AX), 2);
-		reg_w(R_DI) += cpu.EFLAGES.DF?-2:2;
- 	}
-	else{
-		swaddr_write(reg_l(R_EDI), reg_w(R_AX), 2);
-		reg_l(R_EDI) += cpu.EFLAGES.DF?-2:2;
-	}
+	swaddr_write(reg_l(R_EDI), reg_w(R_AX), 2);
+	reg_l(R_EDI) += cpu.EFLAGES.DF?-2:2;
 	
 	print_asm("stosw");
 	return 1;
@@ -32,14 +20,8 @@ make_helper(stos_w)
 
 make_helper(stos_l)
 {
-	if(ops_decoded.is_operand_size_16 == true) {
-		swaddr_write(reg_w(R_DI), reg_l(R_EAX), 4);
-		reg_w(R_DI) += cpu.EFLAGES.DF?-4:4;
-	}
-	else{
-		swaddr_write(reg_l(R_EDI), reg_l(R_EAX), 4);
-		reg_l(R_EDI) += cpu.EFLAGES.DF?-4:4;
-	}
+	swaddr_write(reg_l(R_EDI), reg_l(R_EAX), 4);
+	reg_l(R_EDI) += cpu.EFLAGES.DF?-4:4;
 	
 	print_asm("stosl");
 	return 1;
