309644ede790c4d29c13fc6adeeaf1364eeaacb4
tracer-ics2016 <tracer@njuics.org>
2016-10-15 12:27:03 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:27:03 up 1 day,  2:33,  2 users,  load average: 2.07, 2.02, 2.00 cb3850badc046731f376acc592e655655d9e76c1

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -2,7 +2,7 @@
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
 //	nemu_assert(0);
-	return (a/b) << 16;
+	return (a * b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
@@ -30,7 +30,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT f2F(float a) {
- 	/* You should figure out how to convert `a' into FLOAT without
+  	/* You should figure out how to convert `a' into FLOAT without
 	 * introducing x87 floating point instructions. Else you can
 	 * not run this code in NEMU before implementing x87 floating
 	 * point instructions, which is contrary to our expectation.
@@ -67,7 +67,7 @@ FLOAT sqrt(FLOAT x) {
  	do {
 		dt = F_div_int((F_div_F(x, t) - t), 2);
 		t += dt;
-  	} while(Fabs(dt) > f2F(1e-4));
+   	} while(Fabs(dt) > f2F(1e-4));
 
 	return t;
 }
@@ -80,7 +80,7 @@ FLOAT pow(FLOAT x, FLOAT y) {
 		t2 = F_mul_F(t, t);
 		dt = (F_div_F(x, t2) - t) / 3;
 		t += dt;
- 	} while(Fabs(dt) > f2F(1e-4));
+  	} while(Fabs(dt) > f2F(1e-4));
 
 	return t;
 }
