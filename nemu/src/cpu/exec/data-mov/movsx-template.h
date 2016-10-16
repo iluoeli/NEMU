7a1da9dbc1027525ec01488ebf4b1c23b0464bd4
tracer-ics2016 <tracer@njuics.org>
2016-10-16 09:49:43 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:49:43 up 1 day,  8:22,  2 users,  load average: 2.12, 2.03, 2.01 36bc33a327ee89c6578faa7fd74509cbf755b095

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,7 +5,7 @@
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute()
 {
-	int32_t result = (int32_t)op_src->val;
+	int32_t result = (int32_t)(DATA_TYPE_S)op_src->val;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
