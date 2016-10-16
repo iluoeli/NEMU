9f25fdfa92545d51e3a9ec9d1b82ed953eb5ad7d
tracer-ics2016 <tracer@njuics.org>
2016-10-16 11:42:07 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:42:07 up 1 day,  9:43,  2 users,  load average: 2.02, 2.05, 2.02 712bd5b55471cadbf6a318fdf8125f4739faf027

--- testcase/src/shuixianhua.c
+++ testcase/src/shuixianhua.c
@@ -10,20 +10,16 @@ int main() {
 	int n, n2, n1, n0;
 	int k = 0;
 	for(n = 100; n < 1000; n ++) {
-//		set_bp();
 		if(n == 153)
-			set_bp();
 		n2 = n / 100;
 		n1 = (n / 10) % 10;
 		n0 = n % 10;
 
 		if(n == cube(n2) + cube(n1) + cube(n0)) {
-			set_bp();
 			nemu_assert(n == ans[k]);
 			k ++;
 		}
 	}
-	set_bp();
 	nemu_assert(n == 1000);
 	nemu_assert(k == 4);
 
