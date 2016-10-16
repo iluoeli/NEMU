8f2dce7c05cd9e4df8e4e3cfe9ac2e4650351e71
tracer-ics2016 <tracer@njuics.org>
2016-10-16 11:38:40 +0000

    >  run 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:38:40 up 1 day,  9:39,  2 users,  load average: 2.06, 2.08, 2.03 b524839ade346dccd283e5a664b005094ef4a1dd

--- testcase/src/max.c
+++ testcase/src/max.c
@@ -16,8 +16,7 @@ int main() {
 	int i, j, ans_idx = 0;
 	int loop = 0;
 	for(i = 0; i < NR_DATA; i ++) {
-		for(j = 0; j < NR_DATA; j ++) {
-			set_bp();
+		for(j  = 0; j < NR_DATA; j ++) {
 			nemu_assert(max(test_data[i], test_data[j]) == ans[ans_idx ++]);
 			loop ++;
  		}
