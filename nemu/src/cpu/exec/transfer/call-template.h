0809a73efa73e7dee896fd36f45a8662d23f736d
tracer-ics2016 <tracer@njuics.org>
2016-10-16 03:43:28 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  03:43:28 up 1 day,  5:15,  2 users,  load average: 2.05, 2.02, 2.00 db5be265d8c7faeac83c9024f952ff22698c070

--- nemu/src/cpu/exec/transfer/call-template.h
+++ nemu/src/cpu/exec/transfer/call-template.h
@@ -12,7 +12,19 @@ static void do_execute()
 }
 
 make_instr_helper(i)
-make_instr_helper(rm)
+#endif
+
+
+//call indirect
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_helper(concat(call_rm_, SUFFIX))
+{
+	concat(decode_rm_, SUFFIX)(eip+1);	
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eip);
+	cpu.eip = op_src->val;
+	return 0;
+}
 #endif
 
 
