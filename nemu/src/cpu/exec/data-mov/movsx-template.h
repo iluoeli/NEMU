c9eb0e516a3d3818dcc471b21f461724869cea91
tracer-ics2016 <tracer@njuics.org>
2016-10-16 10:09:52 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:09:52 up 1 day,  8:42,  2 users,  load average: 2.01, 2.01, 2.00 7667eb6e9ca5238043683b208aaa38c5dff1f4

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,7 +5,7 @@
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_helper(concat(movsb_, SUFFIX))
 {
-	int len = concat(decode_rmb2r_, SUFFIX)(eip);
+	int len = concat(decode_rmb2r_, SUFFIX)(eip+1);
 	int32_t result = (int32_t)(int8_t)op_src->val;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
@@ -18,7 +18,7 @@ make_helper(concat(movsb_, SUFFIX))
 #if DATA_BYTE == 4
 make_helper(movsw_l)
 {
-	int len = decode_rmw2r_l(eip);	
+	int len = decode_rmw2r_l(eip+1);	
 	int32_t result = (int32_t)(int16_t)op_src->val;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
