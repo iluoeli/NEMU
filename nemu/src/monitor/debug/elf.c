baa5bbcba213ec6f7ed58a944ae3dbe257ff94c9
tracer-ics2016 <tracer@njuics.org>
2016-10-20 03:01:56 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  03:01:56 up 1 day, 13:41,  2 users,  load average: 2.27, 2.09, 2.02 c3ddba1a3b75ff8274de62c5fbebc33a8dca492

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -56,7 +56,7 @@ void load_elf_tables(int argc, char *argv[]) {
 	int i;
 	for(i = 0; i < elf->e_shnum; i ++) {
 		if(sh[i].sh_type == SHT_SYMTAB && 
-				strcmp(shstrtab + sh[i].sh_name, ".symtab") == 0) {
+	 			strcmp(shstrtab + sh[i].sh_name, ".symtab") == 0) {
 			/* Load symbol table from exec_file */
 			symtab = malloc(sh[i].sh_size);
 			fseek(fp, sh[i].sh_offset, SEEK_SET);
@@ -65,7 +65,7 @@ void load_elf_tables(int argc, char *argv[]) {
 			nr_symtab_entry = sh[i].sh_size / sizeof(symtab[0]);
 		}
 		else if(sh[i].sh_type == SHT_STRTAB && 
-				strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0) {
+	 			strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0) {
 			/* Load string table from exec_file */
 			strtab = malloc(sh[i].sh_size);
 			fseek(fp, sh[i].sh_offset, SEEK_SET);
@@ -85,6 +85,7 @@ void load_elf_tables(int argc, char *argv[]) {
 uint32_t search_elf_obj(char *objName, bool *success)
 {
 	int i=0;
+		printf("execFile:: %s\n", exec_file);
 	for (; i < nr_symtab_entry; ++i){
 		printf("objName:: %s\n", objName);
 		printf("Name:: %d\n", symtab[i].st_name);
