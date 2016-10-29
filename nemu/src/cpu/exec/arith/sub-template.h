a1af2715d2af2a33d30f546f6c6394bc3d58e1fa
tracer-ics2016 <tracer@njuics.org>
2016-10-29 09:47:31 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:47:31 up 2 days,  6:32,  2 users,  load average: 0.05, 0.03, 0.00 2aac24a7bcf2b984401e6b70a20bd7fdd4fa1c4

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -21,7 +21,7 @@ static void do_execute()
 	ret = (ret>>2) ^ ret;
 	ret = (ret>>1) ^ ret;
 	cpu.EFLAGES.PF = ret & 1;
-	cpu.EFLAGES.CF = !((result >> (8*DATA_BYTE)) & 1);
+	cpu.EFLAGES.CF = (op_dest->val < op_src->val);
 	print_asm_template2();
 		    
 }
