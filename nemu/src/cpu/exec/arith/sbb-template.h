8324a0cb8e6f668c6f6da395fc288a79a85de85f
tracer-ics2016 <tracer@njuics.org>
2016-10-29 08:27:00 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:27:00 up 2 days,  5:11,  2 users,  load average: 0.02, 0.01, 0.00 7e74750b9496c7e3c0e4fea4fd8d8649486ce68

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -14,14 +14,14 @@ static void do_execute()
 {
 	uint64_t result = (uint64_t)op_dest->val - ((uint64_t)op_src->val + cpu.EFLAGES.CF);
 	OPERAND_W(op_dest, result);
-	cpu.EFLAGES.OF =  ( !MSB(op_src->val+cpu.EFLAGES.CF) == MSB(op_dest->val) && (MSB(op_dest->val) != MSB(result) ));
+	cpu.EFLAGES.OF =  ( MSB(~(op_src->val+cpu.EFLAGES.CF)) == MSB(op_dest->val) && (MSB(op_dest->val) != MSB(result) ));
 	cpu.EFLAGES.SF = MSB(result);
 	cpu.EFLAGES.ZF = ((DATA_TYPE)result == 0);
 	uint32_t ret = (result>>4) ^ result;
 	ret = (ret>>2) ^ ret;
 	ret = (ret>>1) ^ ret;
 	cpu.EFLAGES.PF = ret & 1;
-	cpu.EFLAGES.CF = (result >> (DATA_BYTE * 8) & 1);
+	cpu.EFLAGES.CF = !(result >> (DATA_BYTE * 8) & 1);
 	print_asm_template2();
 }
 
