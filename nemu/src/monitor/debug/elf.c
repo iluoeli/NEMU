0616d637d3efec0a3ea9dd823db100c306d14bdc
tracer-ics2016 <tracer@njuics.org>
2016-10-31 07:01:49 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  07:01:49 up 4 min,  2 users,  load average: 0.11, 0.24, 0.15 ffdedd98156cd8fe57bc4c91125c33aeaed6cf43

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -97,7 +97,7 @@ uint32_t search_elf_obj(char *objName, bool *success)
 	for (; i < nr_symtab_entry; ++i){
 	//	printf("Name:: %d\n", symtab[i].st_name);
 	//	printf("strtab:: %s\n", strtab+symtab[i].st_name);
-		if (symtab[i].st_info == 17 && strcmp(objName, strtab+symtab[i].st_name) == 0){
+ 		if ((symtab[i].st_info == 17 || symtab[i].st_info == 18) && strcmp(objName, strtab+symtab[i].st_name) == 0){
 			*success = true;
 			return symtab[i].st_value;
 		}
