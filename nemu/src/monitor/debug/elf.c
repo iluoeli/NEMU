228683c2597c45930191842abbcc1606a55230fc
tracer-ics2016 <tracer@njuics.org>
2016-11-03 12:41:25 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:41:25 up  9:43,  2 users,  load average: 0.00, 0.02, 0.00 5137b6f4b3f890d6a018ee17ab355a192b48912

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -139,16 +139,16 @@ static void load_stack_info()
 	for(i=0; ebp != 0; ++i){ 
 			printf("ebp %d:%x\n", i, ebp);
 			statab[i].prev_ebp = swaddr_read(ebp, 4);
-			if(statab[i].prev_ebp != 0) {
+			if((ebp+4) < 0x8000000)
 				statab[i].ret_addr = swaddr_read(ebp+4, 4);
-				for(j=0; j < 4; ++j)
-					statab[i].args[j] = swaddr_read(ebp+8+j*4, 4);
-			}
-			else {
+			else
 				statab[i].ret_addr = 0;
-				for(j=0; j < 4; ++j)
+			for(j=0; j < 4; ++j)
+				if((ebp+8+j*4) < 0x8000000)
+					statab[i].args[j] = swaddr_read(ebp+8+j*4, 4);
+				else
 					statab[i].args[j] = 0;
-			}
+
 			ebp = statab[i].prev_ebp;	
 			++nr_st;
 	}
