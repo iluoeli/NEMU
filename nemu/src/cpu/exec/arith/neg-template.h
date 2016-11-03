1d9f1e979c471f9e80437ac07681de602295e429
tracer-ics2016 <tracer@njuics.org>
2016-11-03 07:46:42 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  07:46:42 up  7:22,  2 users,  load average: 0.00, 0.00, 0.00 6ccf59bfbf8680cb4960786f15238dbc3cc0a4e0

--- nemu/src/cpu/exec/arith/neg-template.h
+++ nemu/src/cpu/exec/arith/neg-template.h
@@ -9,7 +9,7 @@ static void do_execute() {
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
-	cpu.EFLAGES.CF = (op_src->val != 0);
+	cpu.EFLAGES.CF = (op_src->val == 0);
 	print_asm_template1();
 }
 
