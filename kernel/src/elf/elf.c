da65247ce9b47274825a0281fbc94334acf2beea
tracer-ics2016 <tracer@njuics.org>
2016-11-03 09:36:02 +0000

    >  compile kernel 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:36:02 up  8:19,  2 users,  load average: 0.03, 0.02, 0.01 13ee83b8771b3816c5abf08ec04926d74625c63c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -59,12 +59,12 @@ uint32_t loader() {
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
  			 */
 			int margin = ph->p_memsz - ph->p_filesz;
-			uint8_t buf_zero[margin];	
+			uint32_t buf_zero[margin];	
 			int j;
-		//	for (j=0; j < margin; ++j)
-		//		buf_zero[j] = 0;
-			//ramdisk_write(buf_zero, ph->p_vaddr+ph->p_filesz, margin);
-			memcpy((void *)ph->p_vaddr + ph->p_filesz, 0, margin);
+			for (j=0; 4*j < margin; ++j)
+				buf_zero[j] = 0x00000000;
+			ramdisk_write(buf_zero, ph->p_vaddr+ph->p_filesz, margin);
+		//	memcpy((void *)ph->p_vaddr + ph->p_filesz, 0, margin);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
