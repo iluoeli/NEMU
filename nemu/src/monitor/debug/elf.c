ad299e24ebd4ca5908e7e21be8732c447c4ff8a7
tracer-ics2016 <tracer@njuics.org>
2016-11-06 12:36:32 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:36:32 up 11:37,  2 users,  load average: 0.08, 0.02, 0.02 2215e09825b30f0a9b889727e77c03f84f3de49d

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -108,12 +108,12 @@ static void load_func_info()
 	int i = 0;
 	nr_func = 0;
 	for (; i < nr_symtab_entry; ++i){ 
-		printf("%d, %s\n", symtab[i].st_info, strtab+symtab[i].st_name);
-	 	if (symtab[i].st_info == 18){
+		//printf("%d, %s\n", symtab[i].st_info, strtab+symtab[i].st_name);
+	  	if (symtab[i].st_info == 18){
 			func_info[nr_func][2] = i;
 			func_info[nr_func][0] = symtab[i].st_value;
 			func_info[nr_func][1] = func_info[nr_func][0] + symtab[i].st_size;		
-			printf("func_num: %d, func_name: %s\tfunc_value: %x\n", func_info[nr_func][2],symtab[i].st_name+strtab, symtab[i].st_value);
+			//printf("func_num: %d, func_name: %s\tfunc_value: %x\n", func_info[nr_func][2],symtab[i].st_name+strtab, symtab[i].st_value);
 			nr_func ++;
 		}
 	}	
@@ -125,7 +125,7 @@ int is_func(swaddr_t addr)
 	for ( ; i < nr_func; i++){
 		if(addr >= func_info[i][0] && addr <= func_info[i][1])
 			return func_info[i][2];
-	}
+ 	}
 	return -1;
 }
 
@@ -137,7 +137,7 @@ static void load_stack_info()
 	if(ebp == 0)
 		nr_st = 0;
 	for(i=0; ebp != 0; ++i){ 
-			printf("ebp %d:%x\n", i, ebp);
+			//printf("ebp %d:%x\n", i, ebp);
 			statab[i].prev_ebp = swaddr_read(ebp, 4);
 			if((ebp+4) < 0x8000000)
 				statab[i].ret_addr = swaddr_read(ebp+4, 4);
@@ -151,7 +151,7 @@ static void load_stack_info()
 
 			ebp = statab[i].prev_ebp;	
 			++nr_st;
-	}
+ 	}
 }
 
 void print_stack_info()
@@ -167,13 +167,18 @@ void print_stack_info()
 	if(func >= 0) {
 		printf("#0\t0x%x  in  %s(0x%x, 0x%x, 0x%x, 0x%x)  \n", symtab[func].st_value, strtab+symtab[func].st_name, swaddr_read(cpu.ebp+8, 4), swaddr_read(cpu.ebp+12, 4), swaddr_read(cpu.ebp+16, 4), swaddr_read(cpu.ebp+20, 4));
 	}
-	for (i=0; i < nr_st; ++i) {
-		printf("prev_ebp: %x\tret_addr: %x\n", statab[i].prev_ebp, statab[i].ret_addr);
+	for (i=0; i < (nr_st-1); ++i) {
+		//printf("prev_ebp: %x\tret_addr: %x\n", statab[i].prev_ebp, statab[i].ret_addr);
 		func = is_func(statab[i].ret_addr);
 		if((func != -1)){
 		//	printf("func = %d\n", func);
 			printf("#%d\t0x%x  in  %s(%x, %x, %x, %x)  \n", i+1, symtab[func].st_value, strtab+symtab[func].st_name, statab[i+1].args[0], statab[i+1].args[1], statab[i+1].args[2], statab[i+1].args[3]);
-		}
-	}
+ 		}
+ 	}
+/*
+	if(nr_st >= 1){
+		printf("#%d\t0x%x  in  %s(0x%x, 0x%x, 0x%x, 0x%x)  \n", i+1, symtab[func].st_value, strtab+symtab[func].st_name, swaddr_read(cpu.ebp+8, 4), swaddr_read(cpu.ebp+12, 4), swaddr_read(cpu.ebp+16, 4), swaddr_read(cpu.ebp+20, 4));
 
+	}
+*/
 }
