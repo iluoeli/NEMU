02b01aa0fce75428270f1895cea5362d66fc0f09
tracer-ics2016 <tracer@njuics.org>
2016-11-20 05:54:04 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  05:54:04 up 1 day,  3:02,  2 users,  load average: 0.00, 0.00, 0.00 ee1e41d45fcc87d1ba4e2ceaf5c62ccfd45ccf0

--- nemu/src/memory/cacheL2.c
+++ nemu/src/memory/cacheL2.c
@@ -154,9 +154,9 @@ void cacheL2_write(hwaddr_t addr, size_t len, uint32_t data)
 	int j=0;
 	for (; j < len; ++j){
 		if((block + j) > BLOCK_SIZE)	
-			cacheL2_write(addr+j, 1, (data >> (4*j))&0xff);
+			cacheL2_write(addr+j, 1, (data >> (8*j))&0xff);
 		else
-			cacheL2[set][i].data[block+j] =  (data >> (4*j)) & 0xff;
+			cacheL2[set][i].data[block+j] =  (data >> (8*j)) & 0xff;
 	}
 	cacheL2[set][i].dirty = true;
 }
