e3fce550224909123684130d0cb1095f7ed3e01d
tracer-ics2016 <tracer@njuics.org>
2016-11-28 06:38:11 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  06:38:11 up 1 day, 17:17,  2 users,  load average: 0.00, 0.00, 0.00 d66aeda8116b669d85b146d07c3c8f05f91c5eb4

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -68,7 +68,7 @@ uint32_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	assert(sreg <= 5 && sreg >= 0);
 	// mode
 	if(cpu.CR0.PE == 1){
-		printf("protect mod\n");
+		printf("protect mod: %x\n", addr);
 		if(cpu.sr[sreg].TI == 0){
 			uint32_t gdt_addr = cpu.GDTR.base;
 			printf("gdt_addr: %x", gdt_addr);
