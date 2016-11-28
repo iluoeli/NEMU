65070f1ce90f4bae976d424b2af44eb30a0bebdf
tracer-ics2016 <tracer@njuics.org>
2016-11-28 04:47:47 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  04:47:47 up 1 day, 16:09,  2 users,  load average: 0.00, 0.00, 0.00 e63a9a6062e09cc7109951cdcd9299ed68b0dabd

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -42,6 +42,7 @@ make_helper(mov_r2cr)
 {
 	int len = decode_r_l(cpu.eip+2);	
 	cpu.CR0._32 = op_src->val;
+	printf("mov_r2cr: %d\n", op_src->val);
 
 	print_asm("movl" " %%%s, CR0", REG_NAME(op_src->reg));
 	return len+2;
