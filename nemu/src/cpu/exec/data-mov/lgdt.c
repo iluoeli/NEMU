e30ef27ea6cfe8ef62b88884f4935de43020708a
tracer-ics2016 <tracer@njuics.org>
2016-11-28 04:27:32 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  04:27:32 up 1 day, 15:49,  2 users,  load average: 0.00, 0.01, 0.00 9f2ecf6ac4182212561149d5b5971cd21fd2e019

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -5,10 +5,8 @@ make_helper(lgdt)
 	int len = decode_rm_l(cpu.eip+2);
 	uint32_t addr = op_src->addr;
 	uint8_t sreg = op_src->sreg;
-	printf("lgdt: addr = %d, sreg = %d", addr, sreg);
-//	uint32_t addr = instr_fetch(cpu.eip+2, 4);
-	cpu.GDTR.limit = swaddr_read(addr, 2, sreg);
-	cpu.GDTR.base = swaddr_read(addr+2, 4, sreg);
+	cpu.GDTR.base = swaddr_read(addr, 4, sreg);
+	cpu.GDTR.limit = swaddr_read(addr+4, 2, sreg);
 	return len;
 }
 
