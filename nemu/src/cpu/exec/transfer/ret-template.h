d3b36eaf6e83d9d931b4e0e629d35bc61f4fadf0
tracer-ics2016 <tracer@njuics.org>
2016-11-28 08:33:12 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:33:12 up 1 day, 18:43,  2 users,  load average: 0.00, 0.03, 0.07 af1577ffbbf455d19ee4ca7afd8fa22e38c1b43d

--- nemu/src/cpu/exec/transfer/ret-template.h
+++ nemu/src/cpu/exec/transfer/ret-template.h
@@ -8,26 +8,27 @@ static void do_execute()
 	if(ops_decoded.is_operand_size_16) {
 		cpu.eip = MEM_R(REG(R_ESP), 2) & 0x0000ffff;
 		reg_w(R_SP) += 2;
-	}
+	 }
    	else {
 		cpu.eip = MEM_R(REG(R_ESP), 2);
 		reg_l(R_ESP) += 4;
 	}
-//	REG(R_ESP) += DATA_BYTE;	
+//	REG(R_ESP) += DATA_BYTE;
+	cpu.eip -= 1;	
 	print_asm("ret");
 }
 
 make_helper(concat(ret_, SUFFIX))
 {
 	do_execute();	
-	return 0;	
+	return 1;	
 }
 make_helper(ret_i_b)
 {
 	decode_i_b(eip+1);
 	do_execute();
 	reg_l(R_ESP) += op_src->val;	
-	return 0;	
+	return 1;	
 }
 #endif
 
