fc40526fa64514cfa606e347cbda24541655b2d4
tracer-ics2016 <tracer@njuics.org>
2016-12-04 11:41:39 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:41:39 up 14:41,  3 users,  load average: 0.09, 0.22, 0.11 fa96585844b6397fa474d6c0ad9c78faa914da

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -139,7 +139,7 @@ uint32_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 {
 	assert(sreg <= 5 && sreg >= 0);
 	// mode
-/*	if(cpu.CR0.PE == 1){
+	if(cpu.CR0.PE == 1){
 	//	printf("protect mod: %x\n", addr);
 	 	if(cpu.sr[sreg].TI == 0){
 	//		printf("TI == 0\n");
@@ -156,7 +156,7 @@ uint32_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	//		printf("base_addr: %x", base_addr);
 			return (base_addr + offset_addr);
 		}			
-	}*/
+	}
 	return addr;
 }
 
