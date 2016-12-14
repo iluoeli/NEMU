bcb16d4b57c795a56bb8776bdbdefdab09813179
tracer-ics2016 <tracer@njuics.org>
2016-12-14 11:11:21 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:11:21 up 1 day,  1:46,  3 users,  load average: 0.00, 0.00, 0.00 536d11cfd2ba7aa83e8c25602136b292dc9f5b2a

--- nemu/src/cpu/exec/data-mov/int.c
+++ nemu/src/cpu/exec/data-mov/int.c
@@ -74,6 +74,20 @@ make_helper(popa)
 {
 	cpu.edi = swaddr_read(cpu.esp, 4, 1);
 	cpu.esp += 4;		
+	cpu.esi = swaddr_read(cpu.esp, 4, 1);
+	cpu.esp += 4;		
+	cpu.ebp = swaddr_read(cpu.esp, 4, 1);
+	cpu.esp += 4;		
+	//thorow away
+	cpu.esp += 4;		
+	cpu.ebx = swaddr_read(cpu.esp, 4, 1);
+	cpu.esp += 4;		
+	cpu.edx = swaddr_read(cpu.esp, 4, 1);
+	cpu.esp += 4;		
+	cpu.ecx = swaddr_read(cpu.esp, 4, 1);
+	cpu.esp += 4;		
+	cpu.eax = swaddr_read(cpu.esp, 4, 1);
+	cpu.esp += 4;		
 
 	return 1;
 }
