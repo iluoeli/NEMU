779d566227f26676c7fa46361823f006461d4305
tracer-ics2016 <tracer@njuics.org>
2016-12-15 11:24:25 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:24:25 up 1 day, 10:57,  3 users,  load average: 0.12, 0.04, 0.01 8be39489b7cabcc1db45b3d6d88f54cd6b4a9d3e

--- nemu/src/cpu/exec/data-mov/io-template.h
+++ nemu/src/cpu/exec/data-mov/io-template.h
@@ -1,19 +1,46 @@
 #include "cpu/exec/template-start.h"
 
-#define instr in
-static void do_execute()
-{
-	REG(R_EAX) = pio_read(op_src->val, DATA_BYTE);	
-}
+//#define instr in
 
-make_helper(concat(in_, SUFFIX))
+make_helper(concat(in_i2a_, SUFFIX))
 {
 	int len = decode_i_b(cpu.eip+1);
-	do_execute();
+	//do_execute();
+	REG(R_EAX) = pio_read(op_src->val, DATA_BYTE);	
 
 	print_asm("in" str(SUFFIX) "0x%x, %%%s", op_src->val, REG_NAME(R_EAX));
 
 	return len+1;
+}
+
+make_helper(concat(in_d2a_, SUFFIX_))
+{
+	REG(R_EAX) = pio_read(reg_w(R_EDX), DATA_BYTE);	
+
+	print_asm("in" str(SUFFIX) "%DX, %%%s", REG_NAME(R_EAX));
+
+	return 1;
+}
+
+//------instr out-----------
+make_helper(concat(out_a2i_, SUFFIX))
+{
+	int len = decode_i_b(cpu.eip+1);
+	//do_execute();
+	pio_write(op_src->val, DATA_BYTE, REG(R_EAX));	
+
+	print_asm("out" str(SUFFIX) "%%%s, 0x%x", REG_NAME(R_EAX), op_src->val);
+
+	return len+1;
+}
+
+make_helper(concat(out_a2d_, SUFFIX_))
+{
+	pio_write(reg_w(R_EDX), DATA_BYTE, REG(R_EAX));	
+
+	print_asm("out" str(SUFFIX) "%%%s, %DX",  REG_NAME(R_EAX));
+
+	return 1;
 }
 
 
