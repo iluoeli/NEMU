acea36d99a66990b7216fb76ae0bf9c1c12b34fd
tracer-ics2016 <tracer@njuics.org>
2016-12-15 03:43:27 +0000

    >  compile kernel 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  03:43:27 up 1 day,  7:25,  3 users,  load average: 0.00, 0.00, 0.00 a14ebbbadfd53b5f562e904e534bdeab8520bdc2

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -16,9 +16,9 @@ static void sys_ioctl(TrapFrame *tf) {
 
 static void sys_write(TrapFrame *tf)
 {
-	int fd = *(int *)(tf->old_esp+4);		
-	void *buf = *(uint32_t *)(tf->old_esp+8);
-	uint32_t len = *(uint32_t *)(tf->old_esp+12);
+	int fd = *(int *)(tf->old_esp+8);		
+	void *buf = *(uint32_t *)(tf->old_esp+12);
+	uint32_t len = *(uint32_t *)(tf->old_esp+16);
 	assert(fd == 1 || fd == 2);
 	if(fd == 1 || fd == 2){
 		asm volatile (".byte 0xd6" : : "a"(2), "c"(buf), "d"(len));	
