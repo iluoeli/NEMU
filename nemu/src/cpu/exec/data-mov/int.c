b70ea523b071d5b3ea7700b54a15f5360da9a4e7
tracer-ics2016 <tracer@njuics.org>
2016-12-16 07:13:51 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  07:13:51 up 1 day, 14:36,  3 users,  load average: 0.08, 0.02, 0.01 8b50689b10227401d29db27e982e8a7a813e57f0

--- nemu/src/cpu/exec/data-mov/int.c
+++ nemu/src/cpu/exec/data-mov/int.c
@@ -21,6 +21,7 @@ make_helper(int_i_b)
 	cpu.esp -= 4;
 	swaddr_write(cpu.esp, 4, cpu.eip+len+1, 1);
 */
+	printf("int 0x%x", op_src->val);
 	print_asm("int 0x%x", op_src->val);
 
 	//then jmp eroor process
