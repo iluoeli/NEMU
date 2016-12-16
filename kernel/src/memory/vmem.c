03d888b24f937d3a63495f932e63d16ee8beb641
tracer-ics2016 <tracer@njuics.org>
2016-12-16 10:05:30 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:05:30 up 1 day, 17:03,  3 users,  load average: 0.00, 0.00, 0.00 68f5877c9c6cdf0b88597212af4feda04bb42566

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "memory.h"
 #include <string.h>
+#include "x86.h"
 
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
@@ -8,19 +9,39 @@
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
 
+static PTE vptable[(SCR_SIZE / PAGE_SIZE) + 1] align_to_page;
+
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
+	PDE *kpdir = get_updir();	
+	PDE *pdir = va_to_pa(kpdir);
+	PTE *ptable = va_to_pa(vptable);
+
+	uint32_t nr_ptable = SCR_SIZE/PAGE_SIZE + 1;
+	uint32_t pdir_idx = (VMEM_ADDR >> 22) & 0x3ff;
+	uint32_t pframe_addr = VMEM_ADDR+PAGE_SIZE * nr_ptable;
+
+    pdir[pdir_idx].val = make_pde(ptable);
+	ptable += nr_ptable;
+
+    for (; pframe_addr >= VMEM_ADDR; pframe_addr -= PAGE_SIZE) {
+		ptable->val = make_pte(pframe_addr);
+		ptable --;
+	} 
+
+
+
 	panic("please implement me");
 }
 
 void video_mapping_write_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
-	for(i = 0; i < SCR_SIZE / 4; i ++) {
+ 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		buf[i] = i;
 	}
 }
@@ -30,7 +51,7 @@ void video_mapping_read_test() {
 	uint32_t *buf = (void *)VMEM_ADDR;
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		assert(buf[i] == i);
-	}
+ 	}
 }
 
 void video_mapping_clear() {
