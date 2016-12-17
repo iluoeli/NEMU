e25641853dc21e03e6df4ac92d90648435054029
tracer-ics2016 <tracer@njuics.org>
2016-12-17 12:20:23 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:20:23 up 1 day, 23:33,  3 users,  load average: 0.08, 0.02, 0.01 b1dcd91f4a1fc54e915d4c1510b746cefa15e5cc

---
+++ nemu/src/cpu/exec/logic/bt.c
@@ -0,0 +1,10 @@
+#include "cpu/exec/helper.h"
+
+make_helper(bt_r2rm_v)
+{
+	int len = decode_r2rm_l(cpu.eip+1);
+		
+	cpu.EFLAGES.CF = (op_src->val >> op_dest->val) & 1;	
+
+	return 1+len;
+}
---
+++ nemu/src/cpu/exec/logic/bt.h
@@ -0,0 +1,5 @@
+#ifndef _BT_H_
+#define _Bt_H_
+
+make_helper(bt_r2rm_v);
+#endif
