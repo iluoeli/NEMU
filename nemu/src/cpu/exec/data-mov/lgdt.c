4a772cac9162f65e5c0c3a044f9e4cab34a21def
tracer-ics2016 <tracer@njuics.org>
2016-12-17 11:20:02 +0000

    >  compile NEMU 151220069 Luoyl Linux e7000a0fb3ad 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:20:02 up 1 day, 22:32,  3 users,  load average: 0.02, 0.02, 0.00 b21e98fd44c44fffd54bfdcc190c58833b074d51

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -6,8 +6,8 @@ make_helper(lgdt)
 {
 	int len = decode_rm_l(cpu.eip+2);
 	uint32_t addr = op_src->addr;
-	cpu.GDTR.limit = lnaddr_read(addr, 2);
-	cpu.GDTR.base = lnaddr_read(addr+2, 4);
+	cpu.GDTR.limit = swaddr_read(addr, 2, 1);
+	cpu.GDTR.base = swaddr_read(addr+2, 4, 1);
 	print_asm("lgdt %x:%x,gdtr", cpu.GDTR.limit, cpu.GDTR.base);
 	return len + 1;
 }
@@ -16,8 +16,8 @@ make_helper(lidt)
 {
 	int len = decode_rm_l(cpu.eip+2);
 	uint32_t addr = op_src->addr;
-	cpu.IDTR.limit = lnaddr_read(addr, 2);
-	cpu.IDTR.base = lnaddr_read(addr+2, 4);
+	cpu.IDTR.limit = swaddr_read(addr, 2, 1);
+	cpu.IDTR.base = swaddr_read(addr+2, 4, 1);
 	print_asm("lidt %x:%x,idtr", cpu.IDTR.limit, cpu.IDTR.base);
 	return len + 1;
 }
--- nemu/src/cpu/exec/raise_intr.c
+++ nemu/src/cpu/exec/raise_intr.c
@@ -59,7 +59,7 @@ void raise_intr(uint8_t NO)
 		cpu.CS.base = (gdt.base_31_24 << 24) + (gdt.base_23_16 << 16) + gdt.base_15_0;
 		cpu.CS.limit = (gdt.limit_19_16 << 16) + gdt.limit_15_0;
 		cpu.CS.DPL = gdt.privilege_level;
- 	}
+  	}
 
 	cpu.eip = (idt.offset_31_16 << 16) + idt.offset_15_0;
 //	printf("cpu.eip:%x\n", cpu.eip);
